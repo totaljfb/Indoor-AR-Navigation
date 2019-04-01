1dd5fd69d9ae6e93931535806d0d16ae955f4ed8
Jason Zhang <totaljfb@gmail.com>
2019-04-01 17:22:20 -0400

    Atlas indoor positioning implemented

--- app/src/main/java/ng/dat/ar/ARActivity.java
+++ app/src/main/java/ng/dat/ar/ARActivity.java
@@ -2,13 +2,16 @@ package ng.dat.ar;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.hardware.Camera;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.Location;
-import android.location.LocationListener;
+//import android.location.LocationListener;
 import android.location.LocationManager;
 import android.opengl.Matrix;
 import android.os.Build;
@@ -33,7 +36,23 @@ import com.google.android.gms.location.SettingsClient;
 
 import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
 
-public class ARActivity extends AppCompatActivity implements SensorEventListener, LocationListener {
+import com.indooratlas.android.sdk.IALocation;
+import com.indooratlas.android.sdk.IALocationListener;
+import com.indooratlas.android.sdk.IALocationManager;
+import com.indooratlas.android.sdk.IALocationRequest;
+import com.indooratlas.android.sdk.IAOrientationListener;
+import com.indooratlas.android.sdk.IAOrientationRequest;
+import com.indooratlas.android.sdk.IARegion;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import ng.dat.ar.helper.LocationHelper;
+import ng.dat.ar.model.ARPoint;
+
+public class ARActivity extends AppCompatActivity implements IALocationListener, IARegion.Listener, SensorEventListener, IAOrientationListener {
 
     final static String TAG = "ARActivity";
     private SurfaceView surfaceView;
@@ -42,6 +61,8 @@ public class ARActivity extends AppCompatActivity implements SensorEventListener
     private Camera camera;
     private ARCamera arCamera;
     private TextView tvCurrentLocation;
+    private TextView currentHeading;
+    private TextView realTimeDistance;
 
     private SensorManager sensorManager;
     private final static int REQUEST_CAMERA_PERMISSIONS_CODE = 11;
@@ -51,7 +72,7 @@ public class ARActivity extends AppCompatActivity implements SensorEventListener
     private static final long MIN_TIME_BW_UPDATES = 0;//1000 * 60 * 1; // 1 minute
 
     private LocationManager locationManager;
-    public Location location;
+    //public Location location;
     boolean isGPSEnabled;
     boolean isNetworkEnabled;
     boolean locationServiceAvailable;
@@ -60,19 +81,26 @@ public class ARActivity extends AppCompatActivity implements SensorEventListener
 
     private long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
     private long FASTEST_INTERVAL = 2000; /* 2 sec */
+    private DecimalFormat df = new DecimalFormat("###.##");
 
-
+    private IALocationManager mLocationManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ar);
-        startLocationUpdates();
-
+        //startLocationUpdates();
+        mLocationManager = IALocationManager.create(this);
         sensorManager = (SensorManager) this.getSystemService(SENSOR_SERVICE);
         cameraContainerLayout = (FrameLayout) findViewById(R.id.camera_container_layout);
         surfaceView = (SurfaceView) findViewById(R.id.surface_view);
         tvCurrentLocation = (TextView) findViewById(R.id.tv_current_location);
+        currentHeading = (TextView)findViewById(R.id.current_heading);
+        realTimeDistance = (TextView)findViewById(R.id.distance2P1);
         arOverlayView = new AROverlayView(this);
+        IALocationRequest request = IALocationRequest.create();
+        request.setFastestInterval(-1L);
+        request.setSmallestDisplacement(-1f);
+        mLocationManager.requestLocationUpdates(request, ARActivity.this);
     }
 
     @Override
@@ -82,12 +110,16 @@ public class ARActivity extends AppCompatActivity implements SensorEventListener
         requestCameraPermission();
         registerSensors();
         initAROverlayView();
+        mLocationManager.registerRegionListener(this);
+        mLocationManager.registerOrientationListener(new IAOrientationRequest(5.0,5.0), this);
     }
 
     @Override
     public void onPause() {
         releaseCamera();
         super.onPause();
+        mLocationManager.unregisterRegionListener(this);
+        mLocationManager.unregisterOrientationListener(this);
     }
 
     public void requestCameraPermission() {
@@ -104,7 +136,7 @@ public class ARActivity extends AppCompatActivity implements SensorEventListener
                 this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSIONS_CODE);
         } else {
-            initLocationService();
+            //initLocationService();
         }
     }
 
@@ -188,61 +220,61 @@ public class ARActivity extends AppCompatActivity implements SensorEventListener
     public void onAccuracyChanged(Sensor sensor, int i) {
         //do nothing
     }
+//
+//    private void initLocationService() {
+//
+//        if (Build.VERSION.SDK_INT >= 23 &&
+//                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            return;
+//        }
+//
+//        try {
+//            this.locationManager = (LocationManager) this.getSystemService(this.LOCATION_SERVICE);
+//
+//            // Get GPS and network status
+//            this.isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+//            this.isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+//
+//            if (!isNetworkEnabled && !isGPSEnabled) {
+//                // cannot get location
+//                this.locationServiceAvailable = false;
+//            }
+//
+//            this.locationServiceAvailable = true;
 
-    private void initLocationService() {
+//            if (isNetworkEnabled) {
+//                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+//                        MIN_TIME_BW_UPDATES,
+//                        MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+//                if (locationManager != null) {
+//                    location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+//                    updateLatestLocation();
+//                }
+//            }
+//
+//            if (isGPSEnabled) {
+//                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+//                        MIN_TIME_BW_UPDATES,
+//                        MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+//
+//                if (locationManager != null) {
+//                    location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//                    updateLatestLocation();
+//                }
+//            }
+//        } catch (Exception ex) {
+//            Log.e(TAG, ex.getMessage());
+//
+//        }
+//    }
 
-        if (Build.VERSION.SDK_INT >= 23 &&
-                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-
-        try {
-            this.locationManager = (LocationManager) this.getSystemService(this.LOCATION_SERVICE);
-
-            // Get GPS and network status
-            this.isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-            this.isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-
-            if (!isNetworkEnabled && !isGPSEnabled) {
-                // cannot get location
-                this.locationServiceAvailable = false;
-            }
-
-            this.locationServiceAvailable = true;
-
-            if (isNetworkEnabled) {
-                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-                        MIN_TIME_BW_UPDATES,
-                        MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-                if (locationManager != null) {
-                    location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                    updateLatestLocation();
-                }
-            }
-
-            if (isGPSEnabled) {
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                        MIN_TIME_BW_UPDATES,
-                        MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-
-                if (locationManager != null) {
-                    location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                    updateLatestLocation();
-                }
-            }
-        } catch (Exception ex) {
-            Log.e(TAG, ex.getMessage());
-
-        }
-    }
-
-    private void updateLatestLocation() {
-        if (arOverlayView != null && location != null) {
-            arOverlayView.updateCurrentLocation(location);
-            tvCurrentLocation.setText(String.format("lat: %s \nlon: %s \naltitude: %s \n",
-                    location.getLatitude(), location.getLongitude(), location.getAltitude()));
-        }
-    }
+//    private void updateLatestLocation() {
+//        if (arOverlayView != null && location != null) {
+//            arOverlayView.updateCurrentLocation(location);
+//            tvCurrentLocation.setText(String.format("lat: %s \nlon: %s \naltitude: %s \n",
+//                    location.getLatitude(), location.getLongitude(), location.getAltitude()));
+//        }
+//    }
 
     protected void startLocationUpdates() {
 
@@ -263,46 +295,90 @@ public class ARActivity extends AppCompatActivity implements SensorEventListener
         settingsClient.checkLocationSettings(locationSettingsRequest);
 
         // new Google API SDK v11 uses getFusedLocationProviderClient(this)
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            getFusedLocationProviderClient(this).requestLocationUpdates(mLocationRequest, new LocationCallback() {
-                        @Override
-                        public void onLocationResult(LocationResult locationResult) {
-                            // do work here
-                            onLocationChanged(locationResult.getLastLocation());
-                        }
-                    },
-                    Looper.myLooper());
-        }
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            getFusedLocationProviderClient(this).requestLocationUpdates(mLocationRequest, new LocationCallback() {
+//                        @Override
+//                        public void onLocationResult(LocationResult locationResult) {
+//                            // do work here
+//                            onLocationChanged(locationResult.getLastLocation());
+//                        }
+//                    },
+//                    Looper.myLooper());
+//        }
 
     }
-
 
 
     @Override
-    public void onLocationChanged(Location location) {
-        updateLatestLocation();
+    public void onLocationChanged(IALocation location) {
+        arOverlayView.updateCurrentLocation(location.toLocation());
+        tvCurrentLocation.setText(String.format("lat: %s \nlon: %s \naltitude: %s \nerror(m): %s",
+                location.getLatitude(), location.getLongitude(), location.getAltitude(), df.format(location.getAccuracy())));
+        realTimeDistance.setText(String.format("Distance to Point1: %s", df.format(calculateDistance(38.92898198,location.getLatitude(),
+                -77.23964233, location.getLongitude(),50,50))));
+        Log.d("lat: ", String.valueOf(location.getLatitude()));
+
     }
-    // New location has now been determined
-    //LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
     @Override
     public void onStatusChanged(String s, int i, Bundle bundle) {
+        //updateLatestLocation();
+        Log.d("test_entry: ", "sensor value changed");
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mLocationManager.destroy();
+    }
+
+    @Override
+    public void onEnterRegion(IARegion region) {
 
     }
 
     @Override
-    public void onProviderEnabled(String s) {
+    public void onExitRegion(IARegion region) {
 
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
     }
 
     @Override
-    public void onProviderDisabled(String s) {
+    public void onHeadingChanged(long timestamp, double heading) {
+        currentHeading.setText(String.format("Heading: %s", df.format(heading)));
+    }
+
+    @Override
+    public void onOrientationChange(long timestamp, double[] orientation) {
+
+    }
+
+    public static double calculateDistance(double lat1, double lat2, double lon1, double lon2, double el1, double el2){
+
+        final int R = 6371;
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = R * c * 1000; // convert to meters
+
+        double height = el1 - el2;
+
+        distance = Math.pow(distance, 2) + Math.pow(height, 2);
+
+        return Math.sqrt(distance);
 
     }
 }
--- app/src/main/java/ng/dat/ar/AROverlayView.java
+++ app/src/main/java/ng/dat/ar/AROverlayView.java
@@ -9,6 +9,7 @@ import android.location.Location;
 import android.opengl.Matrix;
 import android.view.View;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +26,7 @@ public class AROverlayView extends View {
     private float[] rotatedProjectionMatrix = new float[16];
     private Location currentLocation;
     private List<ARPoint> arPoints;
+    private ARPoint arPoint;
 
 
     public AROverlayView(Context context) {
@@ -33,10 +35,10 @@ public class AROverlayView extends View {
         this.context = context;
 
         //Demo points
-        arPoints = new ArrayList<ARPoint>() {{
-            add(new ARPoint("Point1", 38.93067783, -77.24157307, 98));
-            add(new ARPoint("Point2", 38.93067783, -77.24157307, 78));
-        }};
+//        arPoints = new ArrayList<ARPoint>() {{
+//            add(new ARPoint("Point1", 38.93067783, -77.24157307, 98));
+//            add(new ARPoint("Point2", 38.93067783, -77.24157307, 78));
+//        }};
     }
 
     public void updateRotatedProjectionMatrix(float[] rotatedProjectionMatrix) {
@@ -47,6 +49,13 @@ public class AROverlayView extends View {
     public void updateCurrentLocation(Location currentLocation){
         this.currentLocation = currentLocation;
         this.invalidate();
+        // 1 meter offset test, the offset is an estimation at latitude 40 degree earth surface
+        // One degree of latitude = 111.03 km
+        // One degree of longitude = 85.39 km
+        double lat = currentLocation.getLatitude() + 1.0/111030;
+        double lon = currentLocation.getLongitude() + 1.0/85390;
+        double alt = currentLocation.getAltitude();
+        arPoint = new ARPoint("Point3", lat,lon,alt);
     }
 
     @Override
@@ -63,24 +72,40 @@ public class AROverlayView extends View {
         paint.setColor(Color.RED);
         paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
         paint.setTextSize(60);
+//
+//        for (int i = 0; i < arPoints.size(); i ++) {
+//            float[] currentLocationInECEF = LocationHelper.WSG84toECEF(currentLocation);
+//            float[] pointInECEF = LocationHelper.WSG84toECEF(arPoints.get(i).getLocation());
+//            float[] pointInENU = LocationHelper.ECEFtoENU(currentLocation, currentLocationInECEF, pointInECEF);
+//
+//            float[] cameraCoordinateVector = new float[4];
+//            Matrix.multiplyMV(cameraCoordinateVector, 0, rotatedProjectionMatrix, 0, pointInENU, 0);
+//
+//            // cameraCoordinateVector[2] is z, that always less than 0 to display on right position
+//            // if z > 0, the point will display on the opposite
+//            if (cameraCoordinateVector[2] < 0) {
+//                float x = (0.5f + cameraCoordinateVector[0] / cameraCoordinateVector[3]) * canvas.getWidth();
+//                float y = (0.5f - cameraCoordinateVector[1] / cameraCoordinateVector[3]) * canvas.getHeight();
+//
+//                canvas.drawCircle(x, y, radius, paint);
+//                canvas.drawText(arPoints.get(i).getName(), x - (30 * arPoints.get(i).getName().length() / 2), y - 80, paint);
+//            }
+//        }
+        float[] currentLocationInECEF = LocationHelper.WSG84toECEF(currentLocation);
+        float[] pointInECEF = LocationHelper.WSG84toECEF(arPoint.getLocation());
+        //float[] pointInECEF = LocationHelper.WSG84toECEF(currentLocation);
+        float[] pointInENU = LocationHelper.ECEFtoENU(currentLocation, currentLocationInECEF, pointInECEF);
 
-        for (int i = 0; i < arPoints.size(); i ++) {
-            float[] currentLocationInECEF = LocationHelper.WSG84toECEF(currentLocation);
-            float[] pointInECEF = LocationHelper.WSG84toECEF(arPoints.get(i).getLocation());
-            float[] pointInENU = LocationHelper.ECEFtoENU(currentLocation, currentLocationInECEF, pointInECEF);
+        float[] cameraCoordinateVector = new float[4];
+        Matrix.multiplyMV(cameraCoordinateVector, 0, rotatedProjectionMatrix, 0, pointInENU, 0);
 
-            float[] cameraCoordinateVector = new float[4];
-            Matrix.multiplyMV(cameraCoordinateVector, 0, rotatedProjectionMatrix, 0, pointInENU, 0);
+        // cameraCoordinateVector[2] is z, that always less than 0 to display on right position
+        // if z > 0, the point will display on the opposite
+        float x = (0.5f + cameraCoordinateVector[0] / cameraCoordinateVector[3]) * canvas.getWidth();
+        float y = (0.5f - cameraCoordinateVector[1] / cameraCoordinateVector[3]) * canvas.getHeight();
 
-            // cameraCoordinateVector[2] is z, that always less than 0 to display on right position
-            // if z > 0, the point will display on the opposite
-            if (cameraCoordinateVector[2] < 0) {
-                float x  = (0.5f + cameraCoordinateVector[0]/cameraCoordinateVector[3]) * canvas.getWidth();
-                float y = (0.5f - cameraCoordinateVector[1]/cameraCoordinateVector[3]) * canvas.getHeight();
+        canvas.drawCircle(x, y, radius, paint);
+        canvas.drawText(arPoint.getName(), x - (30 * arPoint.getName().length() / 2), y - 80, paint);
 
-                canvas.drawCircle(x, y, radius, paint);
-                canvas.drawText(arPoints.get(i).getName(), x - (30 * arPoints.get(i).getName().length() / 2), y - 80, paint);
-            }
-        }
     }
 }
